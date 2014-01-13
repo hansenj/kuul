f67bcdb37cc49187423c3c476ed8881692780211
hansenj <hansen.johan@icloud.com>
2014-01-13 13:11:57 +0100

    slidefunktionalitet i JQuery

---
+++ effects.js
@@ -0,0 +1,27 @@
+﻿$(function()
+	{
+		var stickynavtop = $('#stickynav').offset().top;
+
+		var stickynavfunc = function()
+		{
+			var gotop = $(window).scrollTop();
+
+			if (gotop > stickynavtop)
+				{
+					$('#stickynav').css({'position': 'fixed', 'top':0, 'width':1000});
+				}
+				else
+				{
+					$('#stickynav').css({'position': 'relative'});
+				}
+		};
+
+		// Kör allt som en funktion när sidan laddas
+		stickynavfunc();
+
+		// Kör funktionen varje gång vi börja scrolla på nytt
+		$(window).scroll(function()
+			{
+				stickynavfunc();
+			});
+	})();
